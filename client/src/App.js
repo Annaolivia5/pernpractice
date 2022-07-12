f3f50b3e2496298bbb134a3a86a900c47aa172d3
annaweeks <aoweeks@yahoo.com>
2022-07-12 00:58:29 -0700

    7/11

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,42 @@
-import React, { Fragment } from 'react';
-import './App.css';
+// import React, { Fragment } from 'react';
+// import './App.css';
 
-// components
-import InputTodo from './components/InputTodo';
+// // components
+// import InputTodo from './components/InputTodo';
+
+// function App() {
+//   return ( 
+//     <Fragment>
+//       <div className="container">
+//         <InputTodo />
+//       </div>
+//     </Fragment>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React, { Fragment } from "react";
+import "./App.css";
+
+//components
+
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
 
 function App() {
-  return ( 
+  return (
     <Fragment>
-      <div classname="container">
+      <div className="container">
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
-};
+}
 
 export default App;
